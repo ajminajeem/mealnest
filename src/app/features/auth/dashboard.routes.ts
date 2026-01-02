460f2cef13fce93b27ab845f23f0f985fb5aa063
Athira <athiraa.awdar@gmail.com>
2026-01-02 13:30:29 +0530

    theme

--- src/app/features/auth/dashboard.routes.ts
+++ src/app/features/auth/dashboard.routes.ts
@@ -1,72 +1,70 @@
-import { Routes } from "@angular/router";
-import { LoginComponent } from "../../login/login";
-import { Dashboard } from "../../dashboard/dashboard";
-import { Batches } from "../../batches/batches";
-import { Dishes } from "../../dishes/dishes";
-import { Reports } from "../../reports/reports";
-import { UserManagement } from "../../user-management/user-management";
-import { Siteconfigeration } from "../../siteconfigeration/siteconfigeration";
-import { PrinterSettings } from "../../general/printer-settings/printer-settings";
-import { FeatureConfigeration } from "../../general/feature-configeration/feature-configeration";
-import { QrCodeView } from "../../general/qr-code-view/qr-code-view";
-import { BatchDetails } from "../../batch-details/batch-details";
-import { PrintLabels } from "../../print-labels/print-labels";
-import { VersionHistory } from "../../version-history/version-history";
-import { CreateDish } from "../../create-dish/create-dish";
-import { PrintCounter } from "../../print-counter/print-counter";
+import { Routes } from '@angular/router';
+import { LoginComponent } from '../../login/login';
+import { Dashboard } from '../../dashboard/dashboard';
+import { Batches } from '../../batches/batches';
+import { Dishes } from '../../dishes/dishes';
+import { Reports } from '../../reports/reports';
+import { UserManagement } from '../../user-management/user-management';
+import { Siteconfigeration } from '../../siteconfigeration/siteconfigeration';
+import { PrinterSettings } from '../../general/printer-settings/printer-settings';
+import { FeatureConfigeration } from '../../general/feature-configeration/feature-configeration';
+import { QrCodeView } from '../../general/qr-code-view/qr-code-view';
+import { BatchDetails } from '../../batch-details/batch-details';
+import { PrintLabels } from '../../print-labels/print-labels';
+import { VersionHistory } from '../../version-history/version-history';
+import { CreateDish } from '../../create-dish/create-dish';
+import { PrintCounter } from '../../print-counter/print-counter';
+import { ThemeColors } from '../../global-settings/theme-colors/theme-colors';
 // import {ForgotPassword} from "./forgot-password/forgot-password"
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: 'dashboard',
-    component: Dashboard
+    component: Dashboard,
   },
   {
     path: 'batches',
-    component: Batches
+    component: Batches,
   },
   {
     path: 'dishes',
-    component: Dishes
+    component: Dishes,
   },
   {
     path: 'reprorts',
-    component: Reports
+    component: Reports,
   },
-    {
-        path: 'users',
-        component: UserManagement
-    },
-    {
-      path: 'site-configuration',
-      component: Siteconfigeration
-    },
-    {
-      path: 'printer-settings',
-      component: PrinterSettings
-    },
-    {
-      path: 'feature-configuration',
-      component: FeatureConfigeration
-    },
-    {
-      path: 'qr-code-view',
-      component: QrCodeView
-    },
-    {
-      path: 'batch-details',
-      component: BatchDetails
-    },
-    {
-      path: 'print-labels',
-      component: PrintLabels
-    },
-    {path: 'version-history',
-    component: VersionHistory
-    },
-    {path: 'create-dish',
-    component: CreateDish
-    },
-    {path: 'print-counter',
-    component: PrintCounter
-    }
+  {
+    path: 'users',
+    component: UserManagement,
+  },
+  {
+    path: 'site-configuration',
+    component: Siteconfigeration,
+  },
+  {
+    path: 'printer-settings',
+    component: PrinterSettings,
+  },
+  {
+    path: 'feature-configuration',
+    component: FeatureConfigeration,
+  },
+  {
+    path: 'qr-code-view',
+    component: QrCodeView,
+  },
+  {
+    path: 'batch-details',
+    component: BatchDetails,
+  },
+  {
+    path: 'print-labels',
+    component: PrintLabels,
+  },
+  { path: 'version-history', component: VersionHistory },
+  { path: 'create-dish', component: CreateDish },
+  { path: 'print-counter', component: PrintCounter },
+  { path: 'theme-colors', component: ThemeColors },
+  { path: 'theme-colors', component: ThemeColors },
+  { path: 'theme-colors', component: ThemeColors },
 ];
